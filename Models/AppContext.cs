93bfdac7bdc70bfe91ef8f23fab65ae552d69ef0
IgorMal17 <IgorMal17>
2025-05-06 12:03:16 +0500

    Добавьте файлы проекта.

---
+++ Models/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleLoadingIncludeLoad.Models
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<Country> Countries { get; set; } = null!;
+        public DbSet<Position> Positions { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host = localhost; Port = 5432; Database = usersdb3; Username = postgres; Password = 1111");
+        }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLoadingIncludeLoad.Models
+{
+    internal class Country
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int CapitalId { get; set; }
+        // столица страны
+        public City? Capital { get; set; }  
+        public List<Company> Companies { get; set; } = new();
+    }
+}
